aa6a1e1d46e1528407c9ee0dd05218f935e0fc19
pallavi-lingawar <pallavi40340@gmail.com>
2024-04-03 22:39:54 -0700

    framwork_1

---
+++ src/test/java/com/allianz/test/LoginUITest.java
@@ -0,0 +1,18 @@
+package com.allianz.test;
+
+import org.testng.annotations.Test;
+
+public class LoginUITest 
+{
+	@Test(invocationCount = 3)
+	public void titleTest()
+	{
+		System.out.println("title Test");
+	}
+	
+	@Test(priority = 1)
+	public void applicationDescriptionTest()
+	{
+		System.out.println("App info");
+	}
+}
